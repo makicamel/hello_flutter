d4e714e7980e30f362ba8e19cdd8708907dd3d4a
makicamel <unright@gmail.com>
2019-03-17 18:14:49 +0900

    Change 'uploadImage' method from public to instance.

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,28 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _image = image;
     });
+    uploadImage(image);
   }
+
+  Future<String> uploadImage(File file) async {
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    String subDirectoryName = 'images';
+    final StorageReference ref = FirebaseStorage()
+        .ref()
+        .child(subDirectoryName)
+        .child('${timestamp}');
+    final StorageUploadTask uploadTask = ref.putFile(
+        file,
+        StorageMetadata(
+          contentType: "image/jpeg",
+        ));
+    StorageTaskSnapshot snapshot = await uploadTask.onComplete;
+    if (snapshot.error == null) {
+      return await snapshot.ref.getDownloadURL();
+    } else {
+      return 'Something goes wrong';
+    }
+}
 
   @override
   Widget build(BuildContext context){
@@ -71,23 +92,5 @@ class RandomWords {
         style: _biggerFont,
       ),
     );
-  }
-}
-
-Future<String> uploadImage(File file) async {
-  String subDirectoryName = 'images';
-  final StorageReference ref = FirebaseStorage()
-      .ref()
-      .child(subDirectoryName);
-  final StorageUploadTask uploadTask = ref.putFile(
-      file,
-      StorageMetadata(
-        contentType: "image/jpeg",
-      ));
-  StorageTaskSnapshot snapshot = await uploadTask.onComplete;
-  if (snapshot.error == null) {
-    return await snapshot.ref.getDownloadURL();
-  } else {
-    return 'Something goes wrong';
   }
 }
